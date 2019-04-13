f05fd9a51fa3f9416ec5f632997e38e4e90c3ff9
HumayunSheikh <39210393+HumayunSheikh@users.noreply.github.com>
2019-04-13 16:35:58 +0500

    Initial commit

---
+++ app/src/main/java/com/example/phonecode/MessagingActivity.java
@@ -0,0 +1,187 @@
+package com.example.phonecode;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+public class MessagingActivity extends AppCompatActivity {
+    TextView textView;
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private Button chooseImageBtn;
+    private Button uploadBtn;
+    private EditText nameEditText;
+    private EditText descriptionEditText;
+    private ImageView chosenImageView;
+    private ProgressBar uploadProgressBar;
+
+    private Uri mImageUri;
+
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    FirebaseUser firebaseUser;
+    FirebaseAuth firebaseAuth;
+    FirebaseStorage storage;
+
+    private StorageTask mUploadTask;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+        Intent intent = getIntent();
+        String phoneNumber = intent.getExtras().getString("phone");
+        textView = (TextView) findViewById(R.id.myTextView);
+        textView.setText("This is the authenticated phone number " + phoneNumber);
+
+
+        chooseImageBtn = findViewById(R.id.button_choose_image);
+        uploadBtn = findViewById(R.id.uploadBtn);
+        nameEditText = findViewById(R.id.nameEditText);
+        descriptionEditText = findViewById(R.id.descriptionEditText);
+        chosenImageView = findViewById(R.id.chosenImageView);
+        uploadProgressBar = findViewById(R.id.progress_bar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("App").child("Users").child(userID);
+        storage = FirebaseStorage.getInstance();
+        mStorageRef = storage.getReference();
+
+//        mDatabaseRef = FirebaseDatabase.getInstance().getReference("App").child("Profile");
+        mStorageRef = FirebaseStorage.getInstance().getReference("Profile");
+
+
+
+        chooseImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mUploadTask != null && mUploadTask.isInProgress()) {
+                    Toast.makeText(MessagingActivity.this, "An Upload is Still in Progress", Toast.LENGTH_SHORT).show();
+                } else {
+                    uploadFile();
+                }
+            }
+        });
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+
+            Picasso.get().load(mImageUri).into(chosenImageView);
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void uploadFile() {
+        if (mImageUri != null) {
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+
+            uploadProgressBar.setVisibility(View.VISIBLE);
+            uploadProgressBar.setIndeterminate(true);
+
+            mUploadTask = fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    uploadProgressBar.setVisibility(View.VISIBLE);
+                                    uploadProgressBar.setIndeterminate(false);
+                                    uploadProgressBar.setProgress(0);
+                                }
+                            }, 500);
+
+                            Toast.makeText(MessagingActivity.this, "Teacher  Upload successful", Toast.LENGTH_LONG).show();
+                            Teacher upload = new Teacher(nameEditText.getText().toString().trim(),
+                                    taskSnapshot.getMetadata().getReference().getDownloadUrl().toString(),
+                                    descriptionEditText.getText ().toString ());
+
+                            String uploadId = mDatabaseRef.push().getKey();
+                            mDatabaseRef.child("profileImage").setValue(upload);
+
+                            uploadProgressBar.setVisibility(View.INVISIBLE);
+//                            openImagesActivity ();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            uploadProgressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(MessagingActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            uploadProgressBar.setProgress((int) progress);
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "You haven't Selected Any file selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+}
